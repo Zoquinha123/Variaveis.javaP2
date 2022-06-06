fb1a42faa490adb03feb5d100a18b6e53d22571b
Zoquinha123 <gilson.morais@fatec.sp.gov.br>
2022-06-06 17:44:53 +0000

    gravando

---
+++ Variaveis.java
@@ -0,0 +1,12 @@
+public class Variaveis {
+
+    public static void main(String[] args) {
+        int segundaSemana = 5;
+        int terceiraSemana = 2;
+        int quartaSemana = 7;
+
+        System.out.println("A segunda semana tem o valor: " + segundaSemana);
+        System.out.println("A terceira semana tem o valor: " + terceiraSemana);
+        System.out.println("A quarta semana tem o valor: " + quartaSemana);
+    }
+}
